b1acd1ab2e1786891d1e694b5587f73ef378b3d8
Yunus emre akgün <akgunyunus482@gmail.com>
2022-02-18 10:11:10 +0300

    update readme and route

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -6,21 +6,7 @@ const Route = () => {
     // state to track URL and force component to re-render on change
     const [routeInfo, setCurrentPath] = useState({path:window.location.pathname,id:window.location.pathname.split("/")[3] || null});
     console.log(routeInfo);
-    useEffect(() => {
-        // define callback as separate function so it can be removed later with cleanup function
-        const onLocationChange = () => {
-            // update path state to current window URL
-            setCurrentPath(window.location.pathname);
-        }
 
-        // listen for popstate event
-        window.addEventListener('popstate', onLocationChange);
-
-        // clean up event listener
-        return () => {
-            window.removeEventListener('popstate', onLocationChange)
-        };
-    }, [])
 
     return (routeInfo.path === "/holo-next/")
     ? <HomePage sceneId={null}/>
